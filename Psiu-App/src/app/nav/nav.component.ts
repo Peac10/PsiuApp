189ff073d90a66baac7620a1210593d5d04349d2
Peac10 <peac10@gmail.com>
2018-10-11 18:51:21 -0300

    Routes

--- Psiu-App/src/app/nav/nav.component.ts
+++ Psiu-App/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { OnInit, Component } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,7 +12,11 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
   }
@@ -18,20 +24,22 @@ export class NavComponent implements OnInit {
   login() {
     this.authService.login(this.model)
       .subscribe(next => {
-        console.log('Logado com sucesso');
+       this.alertify.success('Logado com sucesso');
       }, error => {
-        console.log(error);
+        this.alertify.error(error);
+      }, () => {
+        this.router.navigate(['/members']);
       });
   }
 
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
   logout() {
     localStorage.removeItem('token');
-    console.log('Desconectado');
+    this.alertify.message('Desconectado');
+    this.router.navigate(['/home']);
   }
 
 }
